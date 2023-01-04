62405c364d803899e3e4d7a393187a9edacf3bfc
Łukasz Burnos <burnoslukasz@gmail.com>
2023-01-04 14:06:36 +0100

    zadanie2.2 do sprawdzenia

---
+++ src/main/java/Grocery.java
@@ -0,0 +1,15 @@
+public class Grocery {
+    public static void main(String[] args) {
+        String firstProductName = "Mleko";
+        String firstProducerName = "Mlekowita";
+        double firstProductPrice = 2.5;
+
+        System.out.println(firstProductName + " " + firstProducerName + " " + firstProductPrice + "zł");
+
+        String secondProductName = "Czekolada";
+        String secondProducerName = "Wedel";
+        double secondProductPrice = 2.19;
+
+        System.out.println(secondProductName + " " + secondProducerName + " " + secondProductPrice + "zł");
+    }
+}
